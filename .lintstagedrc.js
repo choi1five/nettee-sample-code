a36d4b2ddb5de92f4c6c093b586460c7e972d18d
choi1five <cwo1401@gmail.com>
2025-01-17 23:16:30 +0900

    chore: lint-staged eslint 경로 수정 (apps, packages 폴더 내부)

--- .lintstagedrc.js
+++ .lintstagedrc.js
@@ -1,10 +1,13 @@
 export default {
-  //모든 TypeScript/JavaScript 파일에 대한 ESLint 설정
-  "**/*.{js,jsx,ts,tsx}": (files) => {
-    return [`pnpm eslint ${files.join(" ")} --fix`];
+  '(apps|packages)/**/*.{js,jsx,ts,tsx}': files => {
+    // 각 파일이 속한 디렉토리에서 ESLint를 실행
+    const filesWithDirs = files.map(file => {
+      const dir = file.split('/').slice(0, -1).join('/');
+      console.log(`cd ${dir} && pnpm eslint ${file} --fix`);
+      return `cd ${dir} && pnpm eslint ${file} --fix`;
+    });
+    return filesWithDirs;
   },
   // 공통 Prettier 설정
-  "**/*.{ts,tsx,js,jsx,json,md,mdx,css,html,yml,yaml,scss}": [
-    "prettier --write",
-  ],
+  '**/*.{ts,tsx,js,jsx,json,md,mdx,css,html,yml,yaml,scss}': ['prettier --write'],
 };
