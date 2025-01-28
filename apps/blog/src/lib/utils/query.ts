aa5add830bd98d00ace09f501fad667fc20f1dec
choi1five <cwo1401@gmail.com>
2025-01-28 22:06:41 +0900

    refactor: buildQueryString 함수 import 경로 수정

--- apps/blog/src/lib/utils/query.ts
+++
@@ -1,8 +0,0 @@
-export const buildQueryString = (params: Record<string, string | number | null | undefined>) => {
-  const query = new URLSearchParams();
-  Object.entries(params).forEach(([key, value]) => {
-    if (!value) return;
-    query.append(key, String(value));
-  });
-  return query.toString() ? `?${query.toString()}` : '';
-};
