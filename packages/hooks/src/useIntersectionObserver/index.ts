7db04b88cb1d688e97710e7f725f5f1c1b44d37a
choi1five <cwo1401@gmail.com>
2025-01-28 13:08:07 +0900

    refactor: useIntersectionObserver 제네릭 타입 파라미터 명확화, observer callback 함수 분리

--- packages/hooks/src/useIntersectionObserver/index.ts
+++ packages/hooks/src/useIntersectionObserver/index.ts
@@ -1,32 +1,31 @@
 import { useEffect, useState } from 'react';
-
-interface IntersectionObserverParameter {
-  threshold?: number;
-  root?: Element | null;
-  rootMargin?: string;
-  onChange?: (isIntersecting: boolean) => void;
-  triggerOnce?: boolean;
-}
-
 interface IntersectionState {
   isIntersecting: boolean;
   entry?: IntersectionObserverEntry;
 }
 
-interface IntersectionObserverResult<El extends Element> {
-  targetRef: (target: El | null) => void;
+interface IntersectionObserverParameter {
+  threshold?: number;
+  root?: Element | null;
+  rootMargin?: string;
+  onChange?: (intersection: IntersectionState) => void;
+  triggerOnce?: boolean;
+}
+
+interface IntersectionObserverResult<Target extends Element> {
+  targetRef: (target: Target | null) => void;
   isIntersecting: boolean;
   entry?: IntersectionObserverEntry;
 }
 
-export const useIntersectionObserver = <El extends Element>({
-  threshold,
-  root,
-  rootMargin,
+export const useIntersectionObserver = <Target extends Element>({
+  threshold = 0,
+  root = null,
+  rootMargin = '0px',
   onChange,
-  triggerOnce,
-}: IntersectionObserverParameter = {}): IntersectionObserverResult<El> => {
-  const [target, setTarget] = useState<Element | null>(null);
+  triggerOnce = false,
+}: IntersectionObserverParameter = {}): IntersectionObserverResult<Target> => {
+  const [target, setTarget] = useState<Target | null>(null);
   const [intersection, setIntersection] = useState<IntersectionState>({
     isIntersecting: false,
     entry: undefined,
@@ -36,22 +35,22 @@ export const useIntersectionObserver = <El extends Element>({
     if (!target) return;
 
     const element = target;
-    const observer = new IntersectionObserver(
-      ([entry]) => {
-        const isIntersecting = entry?.isIntersecting ?? false;
-        setIntersection({ isIntersecting, entry });
-        onChange?.(isIntersecting);
 
-        if (entry?.isIntersecting && triggerOnce) {
-          observer.unobserve(element);
-        }
-      },
-      {
-        threshold,
-        root,
-        rootMargin,
+    const handleIntersection = ([entry]: IntersectionObserverEntry[]) => {
+      const isIntersecting = entry?.isIntersecting ?? false;
+      setIntersection({ isIntersecting, entry });
+      onChange?.({ isIntersecting, entry });
+
+      if (isIntersecting && triggerOnce) {
+        observer.unobserve(element);
       }
-    );
+    };
+
+    const observer = new IntersectionObserver(handleIntersection, {
+      threshold,
+      root,
+      rootMargin,
+    });
 
     observer.observe(element);
     return () => observer.disconnect();
