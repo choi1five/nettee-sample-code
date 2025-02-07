f8ba2e514562379580b7afdb455ea39db14a944a
choi1five <cwo1401@gmail.com>
2025-02-07 21:33:41 +0900

    feat: loading.tsx 추가

--- apps/blog/src/app/layout.tsx
+++ apps/blog/src/app/layout.tsx
@@ -3,11 +3,11 @@ import '@nettee-sample/ui/globals.css';
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="ko">
-      <body>
-        <header className="mb-8 bg-black p-4">
+      <body className="flex min-h-screen flex-col items-center justify-start">
+        <header className="mb-8 w-full bg-black p-4">
           <h1 className="text-3xl font-bold text-white">Sample_Code</h1>
         </header>
-        <main className="mx-auto max-w-5xl p-4">{children}</main>
+        <main className="mx-auto flex w-full max-w-5xl flex-grow flex-col p-4">{children}</main>
       </body>
     </html>
   );
