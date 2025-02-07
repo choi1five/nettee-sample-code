b9cec1c64db0644e44890533b2844266193c06b8
choi1five <cwo1401@gmail.com>
2025-02-07 21:20:39 +0900

    feat: blog app fetcher base url 경로 수정

--- apps/blog/src/shared/api/http-client.ts
+++ apps/blog/src/shared/api/http-client.ts
@@ -1,5 +1,5 @@
 import { createHttpClient } from '@nettee-sample/http-client';
 
-const BASE_URL = 'http://localhost:3000/api';
+const BASE_URL = 'http://localhost:4000/api';
 
 export const httpClient = createHttpClient({ baseUrl: BASE_URL });
